832a37ec0f246516b7bba21235f1c12c96dd50cc
Faiq <karyoutomoo@gmail.com>
2018-01-11 09:28:11 +0700

    first commit
    NavController, ChatViewController

---
+++ ChitChat/ChitChat/ChatViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChitChat
+//
+//  Created by Faiq on 11/01/18.
+//  Copyright © 2018 The Chit Chat Company. All rights reserved.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
